e50fb85114b647d1a001c45fbbca17dcfeb06fc0
[[Fazla-Rabbi10] <[rabbiakmcc120742@gmail.com]>
2026-05-17 17:22:51 +0600

    db connect and live search

---
+++ API/search_products.php
@@ -0,0 +1,29 @@
+<?php
+
+require_once '../Model/product_Model.php';
+
+if (isset($_GET['search'])) {
+
+    $search = $_GET['search'];
+
+    $productModel = new ProductModel();
+
+    $products = $productModel->searchProducts($search);
+
+    if ($products->num_rows > 0) {
+
+        while ($row = $products->fetch_assoc()) {
+
+            echo "<p>" . $row['name'] . "</p>";
+
+        }
+
+    } else {
+
+        echo "No Product Found";
+
+    }
+
+}
+
+?>
---
+++ VIews/staff/product_search.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Product Search</title>
+</head>
+<body>
+
+    <h2>Search Product</h2>
+
+    <input type="text" id="search" placeholder="Enter product name">
+
+    <div id="result"></div>
+
+    <script src="../../assets/js/product_search.js"></script>
+
+</body>
+</html>
