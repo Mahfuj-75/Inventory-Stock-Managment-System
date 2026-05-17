b97d5845be6effbfd41197723956e1fb462a372c
[Mahfuj Rahman] <[mahfuj.majumder2002@gmail.com]>
2026-05-17 22:07:56 +0600

    func: database connection

---
+++ Config/config.php
@@ -0,0 +1,7 @@
+<?php
+
+session_start();
+
+define('BASE_URL', 'http://localhost/WebTech/WebTech Project/Inventory-System/');
+
+?>
---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+include('Config/database.php');
+
+$db = new Database();
+$conn = $db->connect();
+
+if ($conn) {
+    echo "Database Connected Successfully";
+}
+
+?>
